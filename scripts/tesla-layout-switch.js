8418cc9c2fe9db7ac57649c81b33a71c2ca4133a
cnoss <christian.noss@fh-koeln.de>
2017-11-25 14:48:15 +0100

    Layoutvariante integriert.

---
+++ scripts/tesla-layout-switch.js
@@ -0,0 +1,18 @@
+/* Verschiedene Layouts via Query String
+Basiert auf "Get Query String Parameters with JavaScript" von David Walsh.
+https://davidwalsh.name/query-string-javascript
+----------------------------------------------------------------------------- */
+
+
+var changeLayout = {};
+changeLayout.switch = function() {
+
+    var urlParams = new URLSearchParams(window.location.search);
+    var layout = urlParams.get("layout");
+    document.getElementById("screen").classList.toggle(layout);
+}
+
+// Warten, bis alles geladen ist
+document.addEventListener("DOMContentLoaded", function(event) {
+    changeLayout.switch();
+});
